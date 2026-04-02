f4fedbe1870b8ae8b29d44cc64195a9e5497001c
jyoti339 <jyotishinde3399@gmail.com>
2026-04-02 12:59:15 +0530

    Initial Commit

---
+++ src/main/java/com/employee/config/CustomErrorDecoder.java
@@ -0,0 +1,27 @@
+package com.employee.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.employee.exception.CustomException;
+import com.employee.exception.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import feign.Response;
+import feign.codec.ErrorDecoder;
+
+public class CustomErrorDecoder implements ErrorDecoder {
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+
+        try(InputStream is = response.body().asInputStream()) {
+            ErrorResponse errorResponse = objectMapper.readValue(is, ErrorResponse.class);
+            return new CustomException(errorResponse.getMessage(), errorResponse.getStatus());
+        } catch (IOException e) {
+            throw new CustomException("INTERNAL_SERVER_ERROR");
+        }
+    }
+}
